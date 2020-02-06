5f69517aad33d5a76e0e20f33dce843aeeb4a321
Xi Pan <xipanpost@gmail.com>
2020-02-06 08:17:37 +0100

    Initial commit

--- WebApiNinjectStudio/Infrastructure/INinjectStudio.cs
+++ WebApiNinjectStudio/Infrastructure/INinjectStudio.cs
@@ -7,6 +7,6 @@ namespace WebApiNinjectStudio.Infrastructure
 {
     public interface INinjectStudio
     {
-        public string MyMessage { get; set; }
+        public string MyMessage { get; set; } 
     }
 }
